2541353ad5134fa8ea7f52ecf8d65a7b415c6198
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-17 13:12:50 +0630

    Update authorize.py

--- bot/plugins/authorize.py
+++ bot/plugins/authorize.py
@@ -63,7 +63,8 @@ def _revoke(client, message):
 
 @Client.on_message(filters.private & filters.incoming & filters.text & ~CustomFilters.auth_users)
 async def _token(client, message):
-  token = message.text.split()[-1]
+  code = url.split("?code=")[1].split("&")[0]
+  token = code.split()[-1]
   WORD = len(token)
   if WORD == 73 and token[1] == "/":
     creds = None
@@ -72,7 +73,7 @@ async def _token(client, message):
       try:
         user_id = message.from_user.id
         sent_message = await message.reply_text("🕵️**Checking received code...**", quote=True)
-        creds = flow.step2_exchange(message.text)
+        creds = flow.step2_exchange(code)
         gDriveDB._set(user_id, creds)
         LOGGER.info(f'AuthSuccess: {user_id}')
         await sent_message.edit(Messages.AUTH_SUCCESSFULLY)
