3eeb7a6c4c20bfd89f717165422239f6b2ebe552
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-22 12:12:58 +0630

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -28,7 +28,6 @@ if __name__ == "__main__":
         api_id=APP_ID,
         api_hash=API_HASH,
         plugins=plugins,
-        parse_mode="markdown",
         workdir=DOWNLOAD_DIRECTORY
     )
     LOGGER.info('Starting Bot !')
