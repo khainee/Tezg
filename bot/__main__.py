a3eb11ef9047fdcbcb613ad6ffedfd66f5481579
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-23 14:20:39 +0630

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -14,21 +14,9 @@ logging.basicConfig(
 LOGGER = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-async def main():
-    if os.path.isfile(".restartmsg"):
-        with open(".restartmsg") as f:
-            chat_id, msg_id = map(int, f)
-        try:
-            await bot.edit_message_text(chat_id=chat_id, message_id=msg_id, text="Restarted successfully!")  
-        except Exception as e:
-            await bot.send_message(chat_id='5227230295', text=f"**ERROR:** ```{e}```")
-        os.remove(".restartmsg")
-
 if __name__ == "__main__":
     if not os.path.isdir(DOWNLOAD_DIRECTORY):
         os.makedirs(DOWNLOAD_DIRECTORY)
     LOGGER.info('Starting Bot !')
     bot.run()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
     LOGGER.info('Bot Stopped !')
