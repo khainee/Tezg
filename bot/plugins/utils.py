511631d9a111b7371d2906a16f3a1d296de4a0db
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-03-24 00:06:43 +0630

    Update utils.py

--- bot/plugins/utils.py
+++ bot/plugins/utils.py
@@ -12,6 +12,24 @@ from bot import SUDO_USERS, DOWNLOAD_DIRECTORY, LOGGER, bot
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
+def get_readable_time(seconds):
+    result = ''
+    (days, remainder) = divmod(seconds, 86400)
+    days = int(days)
+    if days != 0:
+        result += f'{days}d'
+    (hours, remainder) = divmod(remainder, 3600)
+    hours = int(hours)
+    if hours != 0:
+        result += f'{hours}h'
+    (minutes, seconds) = divmod(remainder, 60)
+    minutes = int(minutes)
+    if minutes != 0:
+        result += f'{minutes}m'
+    seconds = int(seconds)
+    result += f'{seconds}s'
+    return result
+
 def get_readable_file_size(size_in_bytes):
     if size_in_bytes is None:
         return '0B'
