431173cc05191cba3e57bbf5c8438275ed012835
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-23 13:17:09 +0630

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -43,5 +43,5 @@ except KeyError:
 plugins = dict(root="bot/plugins")
 
 LOGGER.info("Initializing Pyrogram Client")
-bot = Client("G-DriveBot", bot_token=BOT_TOKEN, api_id=APP_ID, api_hash=API_HASH, plugins=plugins, workdir=DOWNLOAD_DIRECTORY)
+bot = Client("G-DriveBot", bot_token=BOT_TOKEN, api_id=APP_ID, api_hash=API_HASH, plugins=plugins, workdir=DOWNLOAD_DIRECTORY).start()
 bot_loop = bot.loop
