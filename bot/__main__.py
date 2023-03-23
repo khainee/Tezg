473a3c1309ec9dff25004c1cfe0fbc6b37215dee
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-23 11:53:42 +0630

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -10,10 +10,21 @@ logging.basicConfig(
 LOGGER = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+async def main():
+    if ospath.isfile(".restartmsg"):
+        with open(".restartmsg") as f:
+            chat_id, msg_id = map(int, f)
+        try:
+            await bot.edit_message_text(chat_id, msg_id, "Restarted successfully!")  
+        except:
+            pass   
+        osremove(".restartmsg")
+
 
 if __name__ == "__main__":
     if not os.path.isdir(DOWNLOAD_DIRECTORY):
         os.makedirs(DOWNLOAD_DIRECTORY)
     LOGGER.info('Starting Bot !')
-    bot.run()
+    bot.run_until_complete(main())
+    bot.run_forever()
     LOGGER.info('Bot Stopped !')
