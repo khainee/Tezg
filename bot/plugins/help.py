8c4e2ad6c9d409922cda330038cc933ee0737fc2
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-23 22:29:26 +0630

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -1,5 +1,6 @@
 import asyncio
 from pyrogram import Client, filters
+from pyrogram.errors import FloodWait, RPCError
 from bot.config import Messages
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from time import time
@@ -22,7 +23,7 @@ async def ping(client, message):
     await sent_message.edit(f'{end_time - start_time} ms')
 
 
-@Client.on_message(filters.private & filters.incoming & filters.command(['help']), group=2)
+@bot.on_message(filters.private & filters.incoming & filters.command(['help']), group=2)
 def _help(client, message):
     client.send_message(chat_id = message.chat.id,
         text = Messages.HELP_MSG[1],
@@ -32,7 +33,7 @@ def _help(client, message):
 
 help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('help+'))
 
-@Client.on_callback_query(help_callback_filter)
+@bot.on_callback_query(help_callback_filter)
 def help_answer(c, callback_query):
     chat_id = callback_query.from_user.id
     message_id = callback_query.message.id
