bdfb1ccce634b83c44000fbe13db95998c3f9702
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-23 11:05:54 +0630

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,12 +1,7 @@
 import os
 import logging
 from pyrogram import Client
-from bot import (
-  APP_ID,
-  API_HASH,
-  BOT_TOKEN,
-  DOWNLOAD_DIRECTORY
-  )
+from bot import bot
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -19,17 +14,6 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 if __name__ == "__main__":
     if not os.path.isdir(DOWNLOAD_DIRECTORY):
         os.makedirs(DOWNLOAD_DIRECTORY)
-    plugins = dict(
-        root="bot/plugins"
-    )
-    app = Client(
-        "G-DriveBot",
-        bot_token=BOT_TOKEN,
-        api_id=APP_ID,
-        api_hash=API_HASH,
-        plugins=plugins,
-        workdir=DOWNLOAD_DIRECTORY
-    )
     LOGGER.info('Starting Bot !')
-    app.run()
+    bot.run()
     LOGGER.info('Bot Stopped !')
