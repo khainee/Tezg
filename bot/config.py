71da1a430a0fd5180ffb04a32b0f77a9ef4755f7
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-23 14:35:56 +0630

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -43,15 +43,15 @@ class Messages:
         "**Join Channel @drivetalkchannel**"
         ]
      
-    RATE_LIMIT_EXCEEDED_MESSAGE = "❗ **Rate Limit Exceeded.**\n__User rate limit exceeded try after 24 hours.__"
+    RATE_LIMIT_EXCEEDED_MESSAGE = """❗ **Rate Limit Exceeded.**\n__User rate limit exceeded try after 24 hours.__"""
     
     FILE_NOT_FOUND_MESSAGE = """❗ **File/Folder not found.**\n__File id - {} Not found. Make sure it\'s exists and accessible by the logged account.__"""
     
-    INVALID_GDRIVE_URL = "❗ **Invalid Google Drive URL**\nMake sure the Google Drive URL is in valid format."
+    INVALID_GDRIVE_URL = """❗ **Invalid Google Drive URL**\nMake sure the Google Drive URL is in valid format."""
     
     COPIED_SUCCESSFULLY = """✅ **Copied successfully.**\n[{}]({}) __({})__"""
     
-    NOT_AUTH = f"🔑 **You have not authenticated me to upload to any account.**\n__Send /{BotCommands.Authorize[0]} to authenticate.__"
+    NOT_AUTH = f"""🔑 **You have not authenticated me to upload to any account.**\n__Send /{BotCommands.Authorize[0]} to authenticate.__"""
     
     DOWNLOADED_SUCCESSFULLY = """📤 **Uploading File...**\n**Filename:** {}\n**Size:** {}"""
     
@@ -61,38 +61,38 @@ class Messages:
     
     DOWNLOADING = """📥 **Downloading File...\nLink:** {}"""
     
-    ALREADY_AUTH = "🔒 **Already authorized your Google Drive Account.**\n__Use /revoke to revoke the current account.__\n__Send me a direct link or File to Upload on Google Drive__"
+    ALREADY_AUTH = """🔒 **Already authorized your Google Drive Account.**\n__Use /revoke to revoke the current account.__\n__Send me a direct link or File to Upload on Google Drive__"""
     
-    FLOW_IS_NONE = f"❗ **Invalid Code**\n__Run {BotCommands.Authorize[0]} first.__"
+    FLOW_IS_NONE = f"""❗ **Invalid Code**\n__Run {BotCommands.Authorize[0]} first.__"""
     
-    AUTH_SUCCESSFULLY = '🔐 **Authorized Google Drive account Successfully.**'
+    AUTH_SUCCESSFULLY = """🔐 **Authorized Google Drive account Successfully.**"""
     
-    INVALID_AUTH_CODE = '❗ **Invalid Code**\n__The code you have sent is invalid or already used before. Generate new one by the Authorization URL__'
+    INVALID_AUTH_CODE = """❗ **Invalid Code**\n__The code you have sent is invalid or already used before. Generate new one by the Authorization URL__"""
     
-    AUTH_TEXT = "⛓️ **To Authorize your Google Drive account visit this [URL]({}) and send the generated code here.**\n__Visit the URL > Allow permissions > you will get a code > copy it > Send it here__"
+    AUTH_TEXT = """⛓️ **To Authorize your Google Drive account visit this [URL]({}) and send the generated code here.**\n__Visit the URL > Allow permissions > you will get a code > copy it > Send it here__"""
     
     DOWNLOAD_TG_FILE = """📥 Downloading File...\nFilename: {}\nSize: {}\nFile Type: {}"""
 
-    PARENT_SET_SUCCESS = '🆔✅ **Custom Folder link set successfully.**\n__Your custom folder id - {}\nUse__ ```/{} clear``` __to clear it.__'
+    PARENT_SET_SUCCESS = """🆔✅ **Custom Folder link set successfully.**\n__Your custom folder id - {}\nUse__ ```/{} clear``` __to clear it.__"""
     
     PARENT_CLEAR_SUCCESS = f"""🆔🚮 **Custom Folder ID Cleared Successfuly.**\n__Use /{BotCommands.SetFolder[0]} (Folder Link)to set it back__."""
     
-    CURRENT_PARENT = "🆔 **Your Current Custom Folder ID - {}**\n__Use__ ```/{} (Folder link)``` __to change it.__"
+    CURRENT_PARENT = """🆔 **Your Current Custom Folder ID - {}**\n__Use__ ```/{} (Folder link)``` __to change it.__"""
     
-    REVOKED = f"🔓 **Revoked current logged account successfully.**\n__Use /{BotCommands.Authorize[0]} to authenticate again and use this bot.__"
+    REVOKED = f"""🔓 **Revoked current logged account successfully.**\n__Use /{BotCommands.Authorize[0]} to authenticate again and use this bot.__"""
     
-    NOT_FOLDER_LINK = "❗ **Invalid folder link.**\n__The link you send its not belong to a folder.__"
+    NOT_FOLDER_LINK = """❗ **Invalid folder link.**\n__The link you send its not belong to a folder.__"""
     
-    CLONING = "🗂️ **Cloning into Google Drive...**\n__G-Drive Link - {}__"
+    CLONING = """🗂️ **Cloning into Google Drive...**\n__G-Drive Link - {}__"""
     
-    PROVIDE_GDRIVE_URL = "**❗ Provide a valid Google Drive URL along with commmand.**\n__Usage - /{} (GDrive Link)__"
+    PROVIDE_GDRIVE_URL = """**❗ Provide a valid Google Drive URL along with commmand.**\n__Usage - /{} (GDrive Link)__"""
     
-    INSUFFICIENT_PERMISSONS = "❗ **You have insufficient permissions for this file.**\n__File id - {}__"
+    INSUFFICIENT_PERMISSONS = """❗ **You have insufficient permissions for this file.**\n__File id - {}__"""
     
-    DELETED_SUCCESSFULLY = "🗑️✅ **File Deleted Successfully.**\n__File deleted permanently !\nFile id - {}__"
+    DELETED_SUCCESSFULLY = """🗑️✅ **File Deleted Successfully.**\n__File deleted permanently !\nFile id - {}__"""
     
-    WENT_WRONG = "⁉️ **ERROR: SOMETHING WENT WRONG**\n__Please try again later.__"
+    WENT_WRONG = """⁉️ **ERROR: SOMETHING WENT WRONG**\n__Please try again later.__"""
     
-    EMPTY_TRASH = "🗑️🚮**Trash Emptied Successfully !**"
+    EMPTY_TRASH = """🗑️🚮**Trash Emptied Successfully !**"""
     
-    PROVIDE_YTDL_LINK = "❗**Provide a valid YouTube-DL supported link.**"
+    PROVIDE_YTDL_LINK = """❗**Provide a valid YouTube-DL supported link.**"""
