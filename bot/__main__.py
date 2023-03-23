a8351257dd871233d2ee59ff93fd3da44e0c5b9d
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-03-23 13:58:22 +0630

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -13,13 +13,6 @@ logging.basicConfig(
 LOGGER = logging.getLogger(__name__)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-@bot.on_message(filters.private & filters.incoming & filters.command(['start']), group=2)
-def _start(client, message):
-    client.send_message(chat_id = message.chat.id,
-        text = tr.START_MSG.format(message.from_user.mention),
-        reply_to_message_id = message.id
-    )
-
 async def main():
     if os.path.isfile(".restartmsg"):
         with open(".restartmsg") as f:
@@ -35,6 +28,6 @@ if __name__ == "__main__":
     if not os.path.isdir(DOWNLOAD_DIRECTORY):
         os.makedirs(DOWNLOAD_DIRECTORY)
     LOGGER.info('Starting Bot !')
-    bot.loop.run_until_complete(main())
-    bot.loop.run_forever()
+    bot.run_until_complete(main())
+    bot.run_forever()
     LOGGER.info('Bot Stopped !')
