8ab6aa9961d26cde35ec0aeda9aae506df6ff7e5
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-03-25 17:30:24 +0630

    Update __init__.py

--- bot/helpers/sql_helper/__init__.py
+++ bot/helpers/sql_helper/__init__.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from bot import DATABASE_URL, LOGGER, BOT_TOKEN
+from bot import DATABASE_URL, LOGGER, G_DRIVE_CLIENT_ID
 
 if DATABASE_URL is None:
     LOGGER.warning("DATABASE_URL is not set. The application cannot function without a database.")
@@ -9,5 +9,5 @@ client = MongoClient(DATABASE_URL)
 db = client["DRIVE_X"]
 parent_id = db["ParentID"]
 gDrive = db['gDriveCreds']
-bot_id = BOT_TOKEN.split(':', 1)[0]
+c_id = BOT_TOKEN.split('-', 1)[0]
 
