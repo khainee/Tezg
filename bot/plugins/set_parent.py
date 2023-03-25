1b52430ec4e1945df998b87e7ed49829cc1ba783
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-03-25 19:21:22 +0630

    Update set_parent.py

--- bot/plugins/set_parent.py
+++ bot/plugins/set_parent.py
@@ -5,7 +5,7 @@ from bot.helpers.gdrive_utils import GoogleDrive
 from bot.helpers.sql_helper import idsDB
 from bot import LOGGER
 
-@Client.on_message(filters.private & filters.incoming & filters.command(BotCommands.SetFolder) & CustomFilters.auth_users)
+@bot.on_message(filters.private & filters.incoming & filters.command(BotCommands.SetFolder) & CustomFilters.auth_users)
 def _set_parent(client, message):
   user_id = message.from_user.id
   if len(message.command) > 1:
