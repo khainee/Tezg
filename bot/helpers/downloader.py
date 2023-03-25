73883dae661e9aceb95b8a1f31e69d5ea1c1e2d3
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-03-25 14:45:37 +0630

    Update downloader.py

--- bot/helpers/downloader.py
+++ bot/helpers/downloader.py
@@ -47,7 +47,7 @@ def utube_dl(link):
     except DownloadError as e:
       return False, str(e)
     for path in glob.glob(os.path.join(DOWNLOAD_DIRECTORY, '*')):
-      if path.endswith(('.avi', '.mov', '.flv', '.wmv', '.3gp','.mpeg', '.webm', '.mp4', '.mkv')) and \
+      if path.endswith(('.avi', '.mov', '.flv', '.wmv', '.3gp','.mpeg', '.webm', '.mp4', '.mkv', '.acc', '.m4a', '.mp3', '.ogg', '.wav')) and \
           path.startswith(ytdl.prepare_filename(meta)):
         return True, path
     return False, 'Something went wrong! No video file exists on server.'
