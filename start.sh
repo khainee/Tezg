eba76c9d520b06161adc7800a55fa3cf19b7b82e
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-04-29 22:20:34 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ aria2c --allow-overwrite=true --auto-file-renaming=true \
        --disk-cache=40M --enable-rpc=true --force-save=true --http-accept-gzip=true \
        --max-connection-per-server=10 --max-concurrent-downloads=10 --max-file-not-found=0 --max-tries=20 \
        --min-split-size=10M --optimize-concurrent-downloads=true --reuse-uri=true \
-       --quiet=true --rpc-max-request-size=1024M \
+       --quiet=false --rpc-max-request-size=1024M \
        --summary-interval=0 --user-agent=Wget/1.12 --disable-ipv6 &
 python3 -m bot
