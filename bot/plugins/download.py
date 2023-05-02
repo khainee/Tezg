516a7b6a6b58a99a492129b47d4b2f994fbcc783
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-05-02 19:30:41 +0630

    Update download.py

--- bot/plugins/download.py
+++ bot/plugins/download.py
@@ -55,7 +55,8 @@ async def _telegram_file(client, message):
   os.remove(file_path)
 
 async def progress(current, total, sent_message, file):
-    progress_bar = f"File name: {file.file_name}\n"
+    progress_bar = "📥 Downloading File...\n"
+    progress_bar+= f"File name: {file.file_name}\n"
     progress_bar+= f"File Type: {file.mime_type}\n"
     progress_bar+= f"File size: {humanbytes(file.file_size)}\n"
     progress_bar+= f"Progress: {current * 100 / total:.1f}%\n"
