abb4d0989fc898e83d79abdffcf6bcae9fab5e1a
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-05-03 14:31:02 +0630

    Update downloader.py

--- bot/helpers/downloader.py
+++ bot/helpers/downloader.py
@@ -20,7 +20,7 @@ async def download_file(url, dl_path, gid, sent_message):
                   progress_bar+= f"File name: {download.name}\n"
                   progress_bar+= f"File size: {humanbytes(download.total_length)}\n"
                   progress_bar+= f"Speed: {humanbytes(download.download_speed)}/s|ETA: {download.eta}\n"
-                  progress_bar+= f"Processed size: {humanbytes(download.completed_length)\n}"
+                  progress_bar+= f"Processed size: {humanbytes(download.completed_length)}\n"
                   progress_bar+= f"Progress: {progress}%"
                 await sent_message.edit(progress_bar)
                 for file in download.files:
