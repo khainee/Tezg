07be95a044a61c3959cc022054e37c1613d9baf9
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-05-04 09:51:28 +0630

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -8,7 +8,16 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 LOGGER = logging.getLogger(__name__)
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+async def main():
+    os.path.isfile(".restartmsg"):
+        with open(".restartmsg") as f:
+            chat_id, msg_id = map(int, f)
+    try:
+        await bot.edit_message_text(chat_id=chat_id, message_id=msg_id, text='Restarted Successfully!')
+    except:
+        pass
+#    await bot.set_bot_commands([
 
 if __name__ == "__main__":
     if not os.path.isdir(DOWNLOAD_DIRECTORY):
@@ -16,5 +25,6 @@ if __name__ == "__main__":
         LOGGER.info(f'Creating {DOWNLOAD_DIRECTORY}')
 
 LOGGER.info('Starting Bot !')
+bot.loop.run_until_complete(main())
 bot.loop.run_forever()
 LOGGER.info('Bot Stopped !')
