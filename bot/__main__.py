6255c2c2efe9d9665f9bbea13f1e87e64d324a75
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-05-05 23:09:39 +0630

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -9,23 +9,27 @@ async def main():
     if os.path.isfile(".restartmsg"):
         with open(".restartmsg") as f:
             chat_id, msg_id = map(int, f)
+        try:
+            await bot.edit_message_text(chat_id=chat_id, message_id=msg_id, text='Restarted Successfully!')
+        except:
+            pass
     try:
-        await bot.edit_message_text(chat_id=chat_id, message_id=msg_id, text='Restarted Successfully!')
-    except:
-        pass
-    await bot.set_bot_commands([
-        BotCommand(f'{BotCommands.Start}', 'Start Command'),
-        BotCommand(f'{BotCommands.Download}', 'Download support link'),
-        BotCommand(f'{BotCommands.Authorize}', 'Authorizing GDrive Account'),
-        BotCommand(f'{BotCommands.SetFolder}', 'Set Custom Upload Folder'),
-        BotCommand(f'{BotCommands.Revoke}', 'Revoke GDrive Account'),
-        BotCommand(f'{BotCommands.Clone}', 'Clone GDrive Files'),
-        BotCommand(f'{BotCommands.Delete}', 'Delete GDrive Files'),
-        BotCommand(f'{BotCommands.EmptyTrash}', 'Empty GDrive Trash'),
-        BotCommand(f'{BotCommands.YtDl}', 'Download yt-dlp support link'),
-        BotCommand(f'{BotCommands.Speed}', 'Test the speed of server'),
-        BotCommand(f'{BotCommands.Stats}', 'Status of the Bot'),
-        BotCommand(f'{BotCommands.Ping}', 'Ping the bot')])
+        await bot.set_bot_commands([
+            BotCommand(f'{BotCommands.Start}', 'Start Command'),
+            BotCommand(f'{BotCommands.Download}', 'Download support link'),
+            BotCommand(f'{BotCommands.Authorize}', 'Authorizing GDrive Account'),
+            BotCommand(f'{BotCommands.SetFolder}', 'Set Custom Upload Folder'),
+            BotCommand(f'{BotCommands.Revoke}', 'Revoke GDrive Account'),
+            BotCommand(f'{BotCommands.Clone}', 'Clone GDrive Files'),
+            BotCommand(f'{BotCommands.Delete}', 'Delete GDrive Files'),
+            BotCommand(f'{BotCommands.EmptyTrash}', 'Empty GDrive Trash'),
+            BotCommand(f'{BotCommands.YtDl}', 'Download yt-dlp support link'),
+            BotCommand(f'{BotCommands.Speed}', 'Test the speed of server'),
+            BotCommand(f'{BotCommands.Stats}', 'Status of the Bot'),
+            BotCommand(f'{BotCommands.Ping}', 'Ping the bot')])
+    except Exception as e:
+        LOGGER.error(f"Error occurred: {e}")
+
 
 if __name__ == "__main__":
     if not os.path.isdir(DOWNLOAD_DIRECTORY):
