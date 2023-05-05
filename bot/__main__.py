ca4f788133d58f80412af8b16685e710aff577a4
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-05-05 22:59:54 +0630

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,7 @@
 import os
 import logging
+from pyrogram.types import BotCommand
+from bot.config import BotCommands
 from bot import bot, DOWNLOAD_DIRECTORY, LOGGER
 from bot.plugins import authorize, copy, delete, download, help, set_parent, utils, speedtest
 
@@ -11,7 +13,19 @@ async def main():
         await bot.edit_message_text(chat_id=chat_id, message_id=msg_id, text='Restarted Successfully!')
     except:
         pass
-#    await bot.set_bot_commands([
+    await bot.set_bot_commands([
+        BotCommand(f'{BotCommands.Start}', 'Start Command'),
+        BotCommand(f'{BotCommands.Download}', 'Download support link'),
+        BotCommand(f'{BotCommands.Authorize}', 'Authorizing GDrive Account'),
+        BotCommand(f'{BotCommands.SetFolder}', 'Set Custom Upload Folder'),
+        BotCommand(f'{BotCommands.Revoke}', 'Revoke GDrive Account'),
+        BotCommand(f'{BotCommands.Clone}', 'Clone GDrive Files'),
+        BotCommand(f'{BotCommands.Delete}', 'Delete GDrive Files'),
+        BotCommand(f'{BotCommands.EmptyTrash}', 'Empty GDrive Trash'),
+        BotCommand(f'{BotCommands.YtDl}', 'Download yt-dlp support link'),
+        BotCommand(f'{BotCommands.Speed}', 'Test the speed of server'),
+        BotCommand(f'{BotCommands.Stats}', 'Status of the Bot'),
+        BotCommand(f'{BotCommands.Ping}', 'Ping the bot')])
 
 if __name__ == "__main__":
     if not os.path.isdir(DOWNLOAD_DIRECTORY):
