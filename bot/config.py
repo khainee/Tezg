1d3b1afbc8cf3ec5346bca16110b4f7dfa8436a6
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-05-07 16:28:54 +0630

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -8,7 +8,7 @@ class config:
     DOWNLOAD_DIRECTORY = "/usr/src/app/downloads/"
     G_DRIVE_CLIENT_ID = "564756005768-5vf0hs66doiptjr9304u9busj2kf4p8d.apps.googleusercontent.com"
     G_DRIVE_CLIENT_SECRET = "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"
- 
+
 
 class BotCommands:
     Start = 'start'
@@ -42,59 +42,59 @@ class Messages:
 
         "**Join Channel @drivetalkchannel**"
         ]
-     
+
     RATE_LIMIT_EXCEEDED_MESSAGE = """❗ **Rate Limit Exceeded.**\n__User rate limit exceeded try after 24 hours.__"""
-    
+
     FILE_NOT_FOUND_MESSAGE = """❗ **File/Folder not found.**\n__File id - {} Not found. Make sure it\'s exists and accessible by the logged account.__"""
-    
+
     INVALID_GDRIVE_URL = """❗ **Invalid Google Drive URL**\nMake sure the Google Drive URL is in valid format."""
-    
+
     COPIED_SUCCESSFULLY = """✅ **Copied successfully.**\n[{}]({}) __({})__"""
-    
+
     NOT_AUTH = f"""🔑 **You have not authenticated me to upload to any account.**\n__Send /{BotCommands.Authorize} to authenticate.__"""
-    
+
     DOWNLOADED_SUCCESSFULLY = '📤 **Uploading File...**\n**Filename:** {}\n**Size:** {}'
-    
+
     UPLOADED_SUCCESSFULLY = """✅ **Uploaded Successfully.**\n[{}]({}) __({})__"""
-    
+
     DOWNLOAD_ERROR = """❗**Downloader Failed**\n{}\n__Link - {}__"""
 
     CLONE_ERROR = """❗**Clone Failed**\n{}\n__Link - {}__"""
-    
+
     DOWNLOADING = """📥 **Downloading File...\nLink:** {}"""
-    
+
     ALREADY_AUTH = """🔒 **Already authorized your Google Drive Account.**\n__Use /revoke to revoke the current account.__\n__Send me a direct link or File to Upload on Google Drive__"""
-    
+
     FLOW_IS_NONE = f"""❗ **Invalid Code**\n__Run {BotCommands.Authorize} first.__"""
-    
+
     AUTH_SUCCESSFULLY = """🔐 **Authorized Google Drive account Successfully.**"""
-    
+
     INVALID_AUTH_CODE = """❗ **Invalid Code**\n__The code you have sent is invalid or already used before. Generate new one by the Authorization URL__"""
-    
+
     AUTH_TEXT = """⛓️ **To Authorize your Google Drive account visit this [URL]({}) and send the generated code here.**\n__Visit the URL > Allow permissions > you will get a code > copy it > Send it here__"""
-    
+
     DOWNLOAD_TG_FILE = """📥 Downloading File...\nFilename: {}\nSize: {}\nFile Type: {}"""
 
     PARENT_SET_SUCCESS = '🆔✅ **Custom Folder link set successfully.**\n__Your custom folder id - {}\nUse__ ```/{} clear``` __to clear it.__'
-    
+
     PARENT_CLEAR_SUCCESS = f"""🆔🚮 **Custom Folder ID Cleared Successfuly.**\n__Use /{BotCommands.SetFolder} (Folder Link)to set it back__."""
-    
+
     CURRENT_PARENT = '🆔 **Your Current Custom Folder ID - {}**\n__Use__ /{} (Folder link) __to change it.__'
-    
+
     REVOKED = f"""🔓 **Revoked current logged account successfully.**\n__Use /{BotCommands.Authorize} to authenticate again and use this bot.__"""
-    
+
     NOT_FOLDER_LINK = """❗ **Invalid folder link.**\n__The link you send its not belong to a folder.__"""
-    
+
     CLONING = """🗂️ **Cloning into Google Drive...**\n__G-Drive Link - {}__"""
-    
+
     PROVIDE_GDRIVE_URL = """**❗ Provide a valid Google Drive URL along with commmand.**\n__Usage - /{} (GDrive Link)__"""
-    
+
     INSUFFICIENT_PERMISSONS = """❗ **You have insufficient permissions for this file.**\n__File id - {}__"""
-    
+
     DELETED_SUCCESSFULLY = """🗑️✅ **File Deleted Successfully.**\n__File deleted permanently !\nFile id - {}__"""
-    
+
     WENT_WRONG = """⁉️ **ERROR: SOMETHING WENT WRONG**\n__Please try again later.__"""
-    
+
     EMPTY_TRASH = """🗑️🚮**Trash Emptied Successfully !**"""
-    
+
     PROVIDE_YTDL_LINK = """❗**Provide a valid YouTube-DL supported link.**"""
