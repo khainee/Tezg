163f4ca41d7ef751fccb3365261e2c4e169d1036
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-05-07 21:28:03 +0630

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ aria2c --allow-overwrite=true --auto-file-renaming=true \
        --max-connection-per-server=10 --max-concurrent-downloads=10 --max-file-not-found=0 --max-tries=20 \
        --min-split-size=10M --optimize-concurrent-downloads=true --reuse-uri=true \
        --rpc-max-request-size=1024M \
-       --summary-interval=0 --user-agent=Wget/1.12 --disable-ipv6 & python3 -m bot
+       --summary-interval=0 --user-agent='Mozilla/5.0 (Linux; Android 11; M2010J19SI) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36' --disable-ipv6 & python3 -m bot
