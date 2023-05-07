58195d98348c7f6a82be49ace1d6fdd4886e8e15
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-05-07 09:31:35 +0630

    Update copy.py

--- bot/plugins/copy.py
+++ bot/plugins/copy.py
@@ -7,16 +7,16 @@ from bot import LOGGER, bot
 
 @bot.on_message(filters.private & filters.incoming & filters.command(BotCommands.Clone) & CustomFilters.auth_users)
 async def _clone(client, message):
-  user_id = message.from_user.id
-  if len(message.command) > 1:
-    link = message.command[1]
-    LOGGER.info(f'Copy:{user_id}: {link}')
-    result, dl_url = await direct_link(link)
-    if result == True:
-      sent_message = await message.reply_text(Messages.CLONING.format(link), quote=True)
-      msg = GoogleDrive(user_id).clone(dl_url)
-      await sent_message.edit(msg)
+    user_id = message.from_user.id
+    if len(message.command) > 1:
+        link = message.command[1]
+        LOGGER.info(f'Copy:{user_id}: {link}')
+        result, dl_url = await direct_link(link)
+        if result == True:
+            sent_message = await message.reply_text(Messages.CLONING.format(link), quote=True)
+            msg = GoogleDrive(user_id).clone(dl_url)
+            await sent_message.edit(msg)
+        else:
+            await sent_message.edit(Messages.CLONE_ERROR.format(dl_url, link))
     else:
-      await sent_message.edit(Messages.CLONE_ERROR.format(dl_url, link))
-  else:
-    await sent_message.edit(Messages.PROVIDE_GDRIVE_URL.format(BotCommands.Clone))
+      await sent_message.edit(Messages.PROVIDE_GDRIVE_URL.format(BotCommands.Clone))
