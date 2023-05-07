10feb104e0a4f4a8377e98d5a7e14c7094c551c8
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-05-07 09:28:46 +0630

    Update utils.py

--- bot/plugins/utils.py
+++ bot/plugins/utils.py
@@ -20,8 +20,6 @@ async def stats(client, message):
             f'<b>OS Uptime:</b> {get_readable_time(time() - boot_time())}\n\n'\
             f'<b>Total Disk Space:</b> {get_readable_file_size(total)}\n'\
             f'<b>Used:</b> {get_readable_file_size(used)} | <b>Free:</b> {get_readable_file_size(free)}\n\n'\
-            f'<b>Upload:</b> {get_readable_file_size(net_io_counters().bytes_sent)}\n'\
-            f'<b>Download:</b> {get_readable_file_size(net_io_counters().bytes_recv)}\n\n'\
             f'<b>CPU:</b> {cpu_percent(interval=0.5)}%\n'\
             f'<b>RAM:</b> {memory.percent}%\n'\
             f'<b>DISK:</b> {disk}%\n\n'\
