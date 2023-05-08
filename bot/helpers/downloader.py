ea9e450ed5d263c6d0a752a0e22ede5752cf553b
Khaine Zay Ye Myint <123251133+BotTutorials@users.noreply.github.com>
2023-05-08 20:34:53 +0630

    Update downloader.py

--- bot/helpers/downloader.py
+++ bot/helpers/downloader.py
@@ -25,7 +25,7 @@ async def download_file(url, dl_path, gid, sent_message):
               await sent_message.edit(progress_bar)
             except:
               pass
-            path = download.file.path
+            path = download.files.path
             if download.is_complete:
               LOGGER.info("Download complete")
               if os.path.exists(path):
